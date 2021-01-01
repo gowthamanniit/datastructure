c39bd757f12611c3055d2b27f33dfbc30d0ce0f6
GOWTHAMAN <40794051+gowthamanniit@users.noreply.github.com>
2021-01-01 13:14:33 +0530

    Add files via upload

---
+++ student database.sql
@@ -0,0 +1,9 @@
+--create database student;
+use student;
+--create table details(sno int,sname varchar(25),mark int)
+--insert into details values(1,'santhiya',99)
+--insert into details values(2,'nivethitha',98)
+--insert into details values(3,'raja',77)
+--insert into details values(4,'rani',19)
+select * from details
+
